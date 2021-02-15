ffe076b6756ea728fa7bbbfffce040aa3f1bd0e6
Dirk Kelderman <dirkkelderman@gmail.com>
2021-02-15 15:49:21 +0100

    second part completed, find a event by id, update it, and delete it

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -43,15 +43,54 @@ eventsRoutes.post('/events', (req, res, next) => {
 })
 
 eventsRoutes.get('/events/:id', (req, res, next) => {
-  res.status(400).json({ message: 'Route works' });
+  
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Event.findById(req.params.id)
+    // .populate('guest')
+  .then(event => {
+    res.status(200).json(event)
+  })
+  .catch( err => {
+    res.status(500).json(err)
+  })
+
 })
 
 eventsRoutes.put('/events/:id', (req, res, next) => {
-  res.status(400).json({ message: 'Route works' });
+  
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Event.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+    res.json({ message: `Project with ${req.params.id} is updated successfully.` });
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  });
+
 })
 
 eventsRoutes.delete('/events/:id', (req, res, next) => {
-  res.status(400).json({ message: 'Route works' });
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Event.findByIdAndRemove(req.params.id)
+    .then( () => {
+      res.json({ message: `Project with ${req.params.id} is deleted successfully.` });
+    } )
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
 })
 
 
