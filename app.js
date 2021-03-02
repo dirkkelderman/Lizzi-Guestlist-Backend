f09cbdec9a10ce026118a5c4232e3a07d5827fce
Dirk Kelderman <dirkkelderman@gmail.com>
2021-03-02 19:32:12 +0100

    added proxy setting

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+app.set('trust proxy', 1)
 
 // Middleware Setup
 app.use(logger('dev'));
